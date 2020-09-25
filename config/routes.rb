8767dbeaccfa12f844c9264e1d9f9563d0681a45
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-09-25 12:14:20 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'messages/index'
+  root to:"messages#index" 
+  #rルートパスへのアクセスがあったらmessagesコントローラーのindexアクションが呼び出される
+  #ルートパスへのアクセスの時はHTTPメソッドを省略できる 自動でgetと判断してくれる
 end
