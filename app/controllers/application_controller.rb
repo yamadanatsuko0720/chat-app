abee4fe6ec2a42105e09a67712c3e4e18a70962e
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-09-26 14:01:12 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #deviseのメソッドでログインしていないユーザーをログイン画面に促すことができる
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #deviseのコントローラーから呼び出された場合はconfigure_permitted_parametersが呼び出される
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to:"messages#index" 
-  #rルートパスへのアクセスがあったらmessagesコントローラーのindexアクションが呼び出される
+  #ルートパスへのアクセスがあったらmessagesコントローラーのindexアクションが呼び出される
   #ルートパスへのアクセスの時はHTTPメソッドを省略できる 自動でgetと判断してくれる
 end
