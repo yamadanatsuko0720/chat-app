d7ba044a236807320c87f1894d2cd25c302ca92e
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-09-28 12:44:41 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,24 @@ class RoomsController < ApplicationController
 
   def new
     @room = Room.new
-    
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+      #配列として受け取ったデータを含むストロングパラメーターを定義/4711
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+    #配列に対して保存を許可したい場合はバリューに[]を入れる
+  end
+
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
   #中間テーブルを使用したアソシエーション/3851
+
+  validates :name, presence: true
+  #チャットルーム新規作成時はルーム名の入力は必ず必要なのでバリデーションをかける
+
 end
