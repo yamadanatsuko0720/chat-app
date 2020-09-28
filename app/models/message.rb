5cee09e834ad7b5feb483370730848733675e7d5
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-09-28 17:50:46 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   #4947
+  has_one_attached :image
+  #Messagesテーブルの各レコードと画像ファイルを管理するimageカラムの紐付け/4763
 
   validates :content, presence: true
   #空の場合はDBに保存しないというバリデーション/4866
